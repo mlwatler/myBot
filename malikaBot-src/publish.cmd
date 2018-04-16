5bbf070b892ce7a965b33cb123572fc487893ccd
mlwatler <malikalwatler@gmail.com>
2018-04-16 09:36:35 -0500

    Added malikaBot files

---
+++ malikaBot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=malikabot-Web-Deploy.pubxml -p:Password=2mj3ukwvnrmqM4KaiqpDtrg0qAwwpmjBD1JwgqXpr0Q33n2vZrygT0wlQmMH
+
